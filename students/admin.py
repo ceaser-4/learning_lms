1ebd5386ba3bd3d87dfd5dd7244b15b7205795df
ceaser-4 <2207469332@qq.com>
2026-01-27 23:41:54 +0800

    feat: Day2 定义学生模型(Model)并完成数据库迁移

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+# 从当前目录(.)的 models 文件里导入 Student 类
+from .models import Student
 
-# Register your models here.
+
+# 装饰器写法 (这是 Python 高级语法，等同于 admin.site.register(Student))
+# 作用：告诉 Django Admin，请在后台帮我管理 Student 这个表
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    # list_display 控制在后台列表页显示哪些列
+    # 如果不写这个，列表页只会显示 __str__ 返回的名字
+    list_display = ('student_id', 'name', 'gender', 'age')
+
+    # search_fields 控制搜索框能搜哪些字段
+    # 方便你在后台快速找人
+    search_fields = ('name', 'student_id')
--- students/models.py
+++ students/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    # 选项配置：性别选择
+    GENDER_CHOICES = (
+        (1, '男'),
+        (2, '女'),
+    )
+
+    # 定义字段
+    name = models.CharField(max_length=50, verbose_name="姓名")
+    student_id = models.CharField(max_length=20, unique=True, verbose_name="学号")
+    gender = models.SmallIntegerField(choices=GENDER_CHOICES, default=1, verbose_name="性别")
+    age = models.IntegerField(default=18, verbose_name="年龄")
+
+    # 这是为了让后台显示更好看，打印对象时直接显示名字
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "学生信息"
+        verbose_name_plural = verbose_name
